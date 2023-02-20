6f111bf1795713b025cfc19f7d6d99dc1f8b3ff1
Julio Benítez <julio.benitez@technogi.com.mx>
2023-02-20 12:00:09 -0600

    added estlint, prettier, husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,73 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true,
+    },
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+    "project": "./tsconfig.json"
+  },
+  "extends": [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+    "plugin:import/recommended"
+  ],
+  "plugins": ["react", "react-hooks", "@typescript-eslint", "jest"],
+  "env": {
+    "browser": true,
+    "es6": true,
+    "jest": true
+  },
+  "settings": {
+    "import/ignore": [
+      "node_modules/react-native/index\\.js$"
+    ]
+  },
+  "rules": {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    "@typescript-eslint/explicit-function-return-type": [
+      "warn",
+      {
+        allowExpressions: true,
+      }
+    ],
+    "arrow-body-style": ["error", "as-needed"],
+    "no-console": ["error", { "allow": ["warn", "error", "info"] }],
+    'react/require-default-props': 0,
+    'react/no-unused-prop-types': 0,
+    "@typescript-eslint/no-use-before-define": ["error", { "variables": false }],
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        "vars": "all",
+        "args": "after-used",
+        "varsIgnorePattern": "[iI]gnored",
+        "argsIgnorePattern": "^_",
+        "ignoreRestSiblings": true
+      }
+    ],
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/explicit-module-boundary-types": ["error", { "allowArgumentsExplicitlyTypedAsAny": true }],
+    "no-nested-ternary": 0,
+    "react/prop-types": 0,
+    // ===== Disable warnings for types and interfaces ======
+    // "@typescript-eslint/no-unused-vars": "off",
+    // "@typescript-eslint/no-unused-vars-experimental": "warn",
+    // "no-unused-vars": "off",
+    // ======================================================
+    "react/jsx-props-no-spreading": 0,
+    "global-require": 0,
+    "@typescript-eslint/dot-notation": 0,
+  }
 };
--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,13 +2,15 @@
  * @format
  */
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import 'react-native'
+import React from 'react'
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import App from '../App'
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer'
 
+// eslint-disable-next-line jest/expect-expect
 it('renders correctly', () => {
-  renderer.create(<App />);
-});
+  renderer.create(<App />)
+})
