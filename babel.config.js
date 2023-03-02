87c825c3802791c125091f9691840a0b0d1dbbbb
Julio Benítez <julio.benitez@technogi.com.mx>
2023-03-02 10:19:40 -0600

    modified progress circle

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,7 @@ module.exports = {
         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
         alias: {}
       }
-    ]
+    ],
+    'react-native-reanimated/plugin',
   ]
 };
--- src/screens/sales/components/product-item.tsx
+++ src/screens/sales/components/product-item.tsx
@@ -1,36 +1,68 @@
-import { StyleSheet } from 'react-native'
-import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import React, { useContext } from 'react'
 import { moderateScale } from 'react-native-size-matters'
+import CircularProgress from 'react-native-circular-progress-indicator'
 
 import { formatNumber } from 'src/utils/helpers'
-import { theme } from 'src/styles'
+import { mainStyle, theme } from 'src/styles'
+import { LocalizationContext } from 'src/context/localization/localization.context'
 
 import { Body, Card, PressableOpacity } from 'src/components'
 import { Product } from '../models/product.model'
 
 const ProductItem = ({ item }: { item: Product }): JSX.Element => {
-  const { name, price, sales, total } = item
+  const { name, price, sales: productSales, total, performance } = item
+
+  const {
+    translations: { sales },
+  } = useContext(LocalizationContext)
+
   const { cardContainer } = styles
+
+  const performanceFormatted = Math.floor(performance * 100)
+
+  const getCircleColor = (value: number): string => {
+    let color = '#fcc735'
+
+    if (value < 0.3) color = '#fc6335'
+    if (value > 0.7) color = '#34eb83'
+
+    return color
+  }
 
   return (
     <Card style={cardContainer}>
-      <PressableOpacity onPress={() => {}}>
-        <Body bold style={{ color: theme.PRIMARY_COLOR }}>
-          {name}
-        </Body>
+      <PressableOpacity containerStyle={{ flexDirection: 'row' }} onPress={() => {}}>
+        <View style={{ flex: 0.3 }}>
+          <CircularProgress
+            value={performanceFormatted}
+            radius={moderateScale(35)}
+            progressValueColor={theme.BODY_TEXT_COLOR}
+            progressValueStyle={mainStyle.textLgStyle()}
+            activeStrokeColor={getCircleColor(performance)}
+            inActiveStrokeColor={getCircleColor(performance)}
+            inActiveStrokeOpacity={0.2}
+          />
+        </View>
 
-        <Body style={{ marginTop: moderateScale(5) }}>
-          <Body bold>Price: </Body>${formatNumber(Number(price.toFixed(2)))}
-        </Body>
+        <View style={{ flex: 0.7 }}>
+          <Body bold style={{ color: theme.PRIMARY_COLOR }}>
+            {name}
+          </Body>
 
-        <Body>
-          <Body bold>No. Sales: </Body>
-          {formatNumber(sales)}
-        </Body>
+          <Body style={{ marginTop: moderateScale(5) }}>
+            <Body bold>{sales.price}: </Body>${formatNumber(Number(price.toFixed(2)))}
+          </Body>
 
-        <Body>
-          <Body bold>Total Sold: </Body>${formatNumber(Number(total.toFixed(2)))}
-        </Body>
+          <Body>
+            <Body bold>{sales.noSales}: </Body>
+            {formatNumber(productSales)}
+          </Body>
+
+          <Body>
+            <Body bold>{sales.totalSold}: </Body>${formatNumber(Number(total.toFixed(2)))}
+          </Body>
+        </View>
       </PressableOpacity>
     </Card>
   )
