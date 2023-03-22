8024895ee39a34c94099d24e598739bacd84d054
Julio Benítez <julio.benitez@technogi.com.mx>
2023-03-22 16:33:18 -0600

    added jest.config

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: "react-native",
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  transformIgnorePatterns: [],
+  setupFiles: [
+    "./src/jest/jest-setup.ts"
+  ],
+}
